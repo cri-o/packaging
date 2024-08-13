866e5dde9311132f8cc8217902161523db7f7faf
Sascha Grunert <sgrunert@redhat.com>
2024-08-13 15:49:03 +0200

    Test with Kubernetes v1.31.0
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- test/scripts/versions.sh
+++ test/scripts/versions.sh
@@ -3,6 +3,6 @@
 # shellcheck disable=SC2034 # variables are used in sourcing scripts
 
 # Package configs
-KUBERNETES_VERSION=v1.30
+KUBERNETES_VERSION=v1.31
 # allows to be set externally e.g. from a Dockerfile
 PROJECT_PATH=${PROJECT_PATH:-prerelease:/main}
